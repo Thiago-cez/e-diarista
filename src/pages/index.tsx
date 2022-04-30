a55dd3655888e1d143cc4762dc5b4d99ed453031
Thiago-Cez <thiago.silva.ts804@gmail.com>
2022-04-30 23:44:56 +0100

    aula2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
 import { Button, Typography } from "@mui/material";
+import { FormElementsContainer } from "ui/styles/pages/index.style";
 
 const Home: NextPage = () => {
   return (
@@ -15,16 +16,22 @@ const Home: NextPage = () => {
           "Preencha seu endereço e veja todos os profissionais proximos a sua localidade."
         }
       />
-      <TextFieldMask
-        mask={"99.999-999"}
-        label={"Digite seu CEP"}
-        fullWidth
-        variant={"outlined"}
-      />
-      <Typography>CEP Inválido</Typography>
-      <Button variant={"contained"} color={"secondary"} sx={{ width: "220px" }}>
-        Buscar
-      </Button>
+      <FormElementsContainer>
+        <TextFieldMask
+          mask={"99.999-999"}
+          label={"Digite seu CEP"}
+          fullWidth
+          variant={"outlined"}
+        />
+        <Typography color={"error"}>CEP Inválido</Typography>
+        <Button
+          variant={"contained"}
+          color={"secondary"}
+          sx={{ width: "220px" }}
+        >
+          Buscar
+        </Button>
+      </FormElementsContainer>
       <UserInformation
         name={"Thiago Cezario"}
         picture={"https://github.com/Thiago-cez.png"}
